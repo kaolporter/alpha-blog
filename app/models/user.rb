0fce0cce90f3d699fa9428a6cb06846abc6ecf3b
kaolporter <124106905+kaolporter@users.noreply.github.com>
2024-08-23 13:18:26 -0600

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates_uniqueness_of :username, presence: true, length: { minimum: 3, maximum: 25 }
-  validates_uniqueness_of :email, presence: true, format: { with: Devise.email_regexp }, length: { maximum: 105 }
+  validates :username, uniqueness: { case_sensitive: false }, presence: true, length: { minimum: 3, maximum: 25 }
+  validates :email, format: URI::MailTo::EMAIL_REGEXP, presence: true, length: { maximum: 105 }
 end
