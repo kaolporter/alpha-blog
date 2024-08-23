069894795d71d2e6f70e663b64f8e3b6f7379bfa
kaolporter <124106905+kaolporter@users.noreply.github.com>
2024-08-23 11:04:32 -0600

    Created users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_21_192457) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_23_165948) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
